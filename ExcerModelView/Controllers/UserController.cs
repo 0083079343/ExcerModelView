8d8125d7dae43a9b9733c760375f8400764e703e
masoud.nadi1594 <masoud.nadi1594@gmail.com>
2024-02-14 16:16:33 +0330

    Inex-List

---
+++ ExcerModelView/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExcerModelView.Models;
+
+namespace ExcerModelView.Controllers
+{
+    public class UserController : Controller
+    {
+        DbMasoudContext db=new DbMasoudContext();
+   
+        public ActionResult Index()
+        {
+            var Index= db.Users.ToList();
+
+            return View(Index);
+        }
+    }
+}
--- ExcerModelView/Models/ViewModelsUser/List.cs
+++ ExcerModelView/Models/ViewModelsUser/List.cs
@@ -5,48 +5,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace ExcerModelView.Models
+namespace ExcerModelView.Models.ViewModelsUser
 {
-    [Table("T_User")]
-    public class User
+    public class List
     {
-        [Key]
-        [Display(Name = "آیدی")]
-        [Required(ErrorMessage = "فیلد {0} اجباری است!")]
-        public int Id { get; set; }
         [Display(Name = "نام")]
         [Required(ErrorMessage = "فیلد {0} اجباری است!")]
         [MaxLength(20)]
         [RegularExpression("^[آ-ی ]+$", ErrorMessage = "فقط کاراکترهای فارسی مورد تایید است!")]
         public string Name { get; set; }
+
+
         [Display(Name = "نام خانوادگی")]
         [Required(ErrorMessage = "فیلد {0} اجباری است!")]
         [MaxLength(30)]
         [RegularExpression("^[آ-ی ]+$", ErrorMessage = "فقط کاراکترهای فارسی مورد تایید است!")]
         public string Family { get; set; }
+        [Display(Name = "تاریخ عضویت")]
+        [Required(ErrorMessage = "فیلد {0} اجباری است!")]
+        public bool IsActive { get; set; } 
+
+
         [Display(Name = "تاریخ تولد")]
         [Required(ErrorMessage = "فیلد {0} اجباری است!")]
         public DateTime Birthdate { get; set; }
+
+
         [Display(Name = "شماره موبایل")]
         [Required(ErrorMessage = "فیلد {0} اجباری است!")]
         [MaxLength(15)]
         [RegularExpression("(09)[0-9]{9}", ErrorMessage = "فرمت شماره موبایل صحیح نیست!")]
         public string Phone { get; set; }
+
+
         [Display(Name = "کدملی")]
         [Required(ErrorMessage = "فیلد {0} اجباری است!")]
         [MaxLength(10)]
         public string Ncode { get; set; }
-
-        [Display(Name = "رمز عبور")]
-        [Required(ErrorMessage = "فیلد {0} اجباری است!")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-        [Display(Name = "وضعیت")]
-        [Required(ErrorMessage = "فیلد {0} اجباری است!")]
-        public bool IsActive { get; set; }
-        [Display(Name = "تاریخ عضویت")]
-        [Required(ErrorMessage = "فیلد {0} اجباری است!")]
-        public DateTime RegisterDate { get; set; }
 
 
     }
