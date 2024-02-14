95929dbb8e24513c175c74311d23f27b77493c5a
masoud.nadi1594 <masoud.nadi1594@gmail.com>
2024-02-14 15:47:16 +0330

    Add project files.

---
+++ ExcerModelView/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ExcerModelView.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExcerModelView.Models.DbMasoudContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ExcerModelView.Models.DbMasoudContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ ExcerModelView/Models/DbMasoudContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ExcerModelView.Models
+{
+    public class DbMasoudContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+}
